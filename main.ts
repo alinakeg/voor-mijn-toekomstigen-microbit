41b05aa8385c9faa48c48d556a06532ac526c661
alinakeg <151366128+alinakeg@users.noreply.github.com>
2023-11-18 13:10:15 +0100

    ik heb een spel schaar steen papier gemaakt

--- main.ts
+++ main.ts
@@ -9,12 +9,35 @@ input.onButtonPressed(Button.A, function () {
             . . . . .
             `)
     } else if (temperatuur > 15) {
-        basic.showString("goed weer ")
+        basic.showString(" goed weer ")
         basic.showIcon(IconNames.TShirt)
     } else if (temperatuur < 15) {
         basic.showString("brr koud ")
     }
 })
+input.onGesture(Gesture.Shake, function () {
+    tool = randint(0, 2)
+    if (tool == 0) {
+        basic.showLeds(`
+            . . # # #
+            . # # . #
+            # # . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (tool == 1) {
+        basic.showLeds(`
+            . # # . .
+            # # # # .
+            # # # # #
+            # # # # #
+            . # # # .
+            `)
+    } else {
+        basic.showIcon(IconNames.Scissors)
+    }
+})
+let tool = 0
 let temperatuur = 0
 basic.showString("Hello!")
 basic.forever(function () {
